4f4e6693dd1b35d517e6cd2cbf825e1f49990d66
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-11-11 08:39:43 -0700

    feat: Animate health bar.

--- Tower Defense/Assets/_Project/Scripts/UI/BarController.cs
+++ Tower Defense/Assets/_Project/Scripts/UI/BarController.cs
@@ -7,17 +7,33 @@ public class BarController : MonoBehaviour
     private Animator _animator;
     [SerializeField]
     private Image _image;
+    public float Duration = 1;
+    private float _startTime = 0;
+    private float _targetValue = 1;
+    private float _currentValue = 1;
+    private float _startValue = 1;
     public float Value 
     {
-        get => _image.rectTransform.localScale.x;
+        get => _targetValue;
         set 
         {
-            Vector3 scale = _image.rectTransform.localScale;
-            scale.x = value;
-            _image.rectTransform.localScale = scale; 
+            _startTime = Time.time;
+            _startValue = _currentValue;
+            _targetValue = value;
             _animator.SetTrigger("Show");
         }
     }
+
+    public void Update()
+    {
+        float percent = Mathf.Clamp01((Time.time - _startTime) / Duration);
+        _currentValue = Mathf.Lerp(_startValue, _targetValue, percent);
+        Vector3 scale = _image.rectTransform.localScale;
+        scale.x = _currentValue;
+        _image.rectTransform.localScale = scale; 
+    }
+
+    
 
     public void RenderEnemyHealth(EnemyController enemyController)
     {
