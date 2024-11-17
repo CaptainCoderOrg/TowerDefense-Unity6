3f44aa1078e92623bd7dfa86d5c2509431ba6a81
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-11-17 08:00:05 -0700

    feat: Add simple zoom in / out.

---
+++ Tower Defense/Assets/_Project/Scripts/UserInput/CameraZoomController.cs
@@ -0,0 +1,60 @@
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraZoomController : MonoBehaviour
+{   
+    [field: SerializeField]
+    public float MinZoom { get; private set; } = 3;
+    [field: SerializeField]
+    public float MaxZoom { get; private set; } = 10;
+    [field: SerializeField]
+    public float ZoomScale { get; private set; } = .2f;
+    [field: SerializeField]
+    public float Speed { get; private set; } = 0.5f;
+    [SerializeField]
+    private float _differenceEpsilon = .01f;
+
+    [SerializeField]
+    private float _zoomLevel;
+    public float ZoomLevel 
+    { 
+        get => _zoomLevel; 
+        private set
+        {
+            _zoomLevel = Mathf.Clamp(value, MinZoom, MaxZoom);
+        }
+    }
+
+    void Awake()
+    {
+        ZoomLevel = Camera.main.orthographicSize;
+    }
+
+    void Update()
+    {
+        float currentSize = Camera.main.orthographicSize;
+        float difference = currentSize - ZoomLevel;
+        if (Mathf.Abs(difference) > _differenceEpsilon)
+        {
+            Camera.main.orthographicSize -= difference * Speed * Time.deltaTime;
+        } 
+        else
+        {
+            Camera.main.orthographicSize = ZoomLevel;
+        }
+    }
+
+    public void Zoom(float amount)
+    {
+        ZoomLevel -= amount * ZoomScale; 
+    }
+    
+    public void Zoom(InputAction.CallbackContext callbackContext)
+    {
+        if (callbackContext.phase == InputActionPhase.Performed) 
+        { 
+            Zoom(callbackContext.ReadValue<float>());
+        }
+    }
+}
