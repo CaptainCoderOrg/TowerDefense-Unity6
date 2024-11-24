efaf90a3f9a39af95dd8e6435b48a7cfaec1e1b0
Joseph Collard <3529380+jcollard@users.noreply.github.com>
2024-11-24 09:39:53 -0700

    chore: Build and deploy.

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "Captain Coder's Academy-Tower Defense-0.1.0";
 const contentToCache = [
-    "Build/Web.loader.js",
-    "Build/Web.framework.js",
-    "Build/Web.data",
-    "Build/Web.wasm",
+    "Build/d439434c2ac2be13a5cef8c1f87315e5.loader.js",
+    "Build/7a8911b2b8aa458db4065baf594d2161.framework.js",
+    "Build/4c7da61276108b993e86ee761f687954.data",
+    "Build/ff0bcaeaf61ba05fd67d2636f56679b3.wasm",
     "TemplateData/style.css"
 
 ];
